e9f76ba57be88477e5c196b02af0d234d41165e2
panteslav <panteslav@users.noreply.github.com>
2020-08-31 21:23:06 +0300

    stylish formatter refactored

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 const BASE_INDENT_SIZE = 4;
+const symbolCompensationIndent = 2;
 
 const printValue = (value, indentSize) => {
   if (typeof value !== 'object') {
@@ -8,8 +9,8 @@ const printValue = (value, indentSize) => {
   }
 
   const keys = Object.keys(value);
-  const textIndent = _.repeat(' ', indentSize);
   const bracketIndent = _.repeat(' ', indentSize - BASE_INDENT_SIZE);
+  const textIndent = _.repeat(' ', indentSize);
 
   let result = '';
 
@@ -22,53 +23,34 @@ const printValue = (value, indentSize) => {
 const printTree = (tree, indentSize = BASE_INDENT_SIZE) => {
   let result = '';
   const keys = Object.keys(tree);
-  const textIndent = _.repeat(' ', indentSize - 2);
   const bracketIndent = _.repeat(' ', indentSize - BASE_INDENT_SIZE);
+  const textIndent = _.repeat(' ', indentSize - symbolCompensationIndent);
 
   keys.forEach((key) => {
     const currentKey = tree[key];
+    const currentIndent = indentSize + BASE_INDENT_SIZE;
 
     if (_.has(currentKey, 'children')) {
-      result += `${textIndent}  ${key}: ${printTree(
-        currentKey.children,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
+      result += `${textIndent}  ${key}: ${printTree(currentKey.children, currentIndent)}`;
       return;
     }
 
-    if (currentKey.status === '-') {
-      result += `${textIndent}- ${key}: ${printValue(
-        currentKey.value1,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
-      return;
-    }
-
-    if (currentKey.status === '+') {
-      result += `${textIndent}+ ${key}: ${printValue(
-        currentKey.value2,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
-      return;
-    }
-
-    if (currentKey.status === '=') {
-      result += `${textIndent}  ${key}: ${printValue(
-        currentKey.value1,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
-      return;
-    }
-
-    if (currentKey.status === '≠') {
-      result += `${textIndent}- ${key}: ${printValue(
-        currentKey.value1,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
-      result += `${textIndent}+ ${key}: ${printValue(
-        currentKey.value2,
-        indentSize + BASE_INDENT_SIZE,
-      )}`;
+    switch (currentKey.status) {
+      case '-':
+        result += `${textIndent}- ${key}: ${printValue(currentKey.value1, currentIndent)}`;
+        return;
+      case '+':
+        result += `${textIndent}+ ${key}: ${printValue(currentKey.value2, currentIndent)}`;
+        return;
+      case '=':
+        result += `${textIndent}  ${key}: ${printValue(currentKey.value1, currentIndent)}`;
+        return;
+      case '≠':
+        result += `${textIndent}- ${key}: ${printValue(currentKey.value1, currentIndent)}`;
+        result += `${textIndent}+ ${key}: ${printValue(currentKey.value2, currentIndent)}`;
+        return;
+      default:
+        throw new Error('wrong key status in the diffTree');
     }
   });
 
