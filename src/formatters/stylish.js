1a75c6f14f3699c7775c2b985447240be2e77423
panteslav <panteslav@users.noreply.github.com>
2020-08-31 22:45:20 +0300

    additional stylish formatter refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -20,6 +20,22 @@ const printValue = (value, indentSize) => {
   return `{\n${result}${bracketIndent}}\n`;
 };
 
+const printEntry = (tree, key, textIndent, indentSize) => {
+  const currentKey = tree[key];
+  const value1 = _.has(currentKey, 'value1') ? printValue(currentKey.value1, indentSize) : null;
+  const value2 = _.has(currentKey, 'value2') ? printValue(currentKey.value2, indentSize) : null;
+  switch (currentKey.status) {
+    case '-':
+      return `${textIndent}- ${key}: ${value1}`;
+    case '+':
+      return `${textIndent}+ ${key}: ${value2}`;
+    case '=':
+      return `${textIndent}  ${key}: ${value1}`;
+    default:
+      return `${textIndent}- ${key}: ${value1}${textIndent}+ ${key}: ${value2}`;
+  }
+};
+
 const printTree = (tree, indentSize = BASE_INDENT_SIZE) => {
   let result = '';
   const keys = Object.keys(tree);
@@ -28,30 +44,11 @@ const printTree = (tree, indentSize = BASE_INDENT_SIZE) => {
 
   keys.forEach((key) => {
     const currentKey = tree[key];
-    const currentIndent = indentSize + BASE_INDENT_SIZE;
+    const currentIndentSize = indentSize + BASE_INDENT_SIZE;
 
     if (_.has(currentKey, 'children')) {
-      result += `${textIndent}  ${key}: ${printTree(currentKey.children, currentIndent)}`;
-      return;
-    }
-
-    switch (currentKey.status) {
-      case '-':
-        result += `${textIndent}- ${key}: ${printValue(currentKey.value1, currentIndent)}`;
-        return;
-      case '+':
-        result += `${textIndent}+ ${key}: ${printValue(currentKey.value2, currentIndent)}`;
-        return;
-      case '=':
-        result += `${textIndent}  ${key}: ${printValue(currentKey.value1, currentIndent)}`;
-        return;
-      case '≠':
-        result += `${textIndent}- ${key}: ${printValue(currentKey.value1, currentIndent)}`;
-        result += `${textIndent}+ ${key}: ${printValue(currentKey.value2, currentIndent)}`;
-        return;
-      default:
-        throw new Error('wrong key status in the diffTree');
-    }
+      result += `${textIndent}  ${key}: ${printTree(currentKey.children, currentIndentSize)}`;
+    } else result += printEntry(tree, key, textIndent, currentIndentSize);
   });
 
   return `{\n${result}${bracketIndent}}\n`;
