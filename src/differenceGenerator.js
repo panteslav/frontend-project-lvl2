296a2fd22b6fe0e7cb6e0e8f8c4b583603a3410d
panteslav <panteslav@users.noreply.github.com>
2020-09-01 20:48:42 +0300

    code smells fix

--- src/differenceGenerator.js
+++ src/differenceGenerator.js
@@ -1,16 +1,5 @@
 import _ from 'lodash';
 
-const createSetOfAllKeys = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  const allUniqueKeys = new Set();
-  keys1.forEach((key) => allUniqueKeys.add(key));
-  keys2.forEach((key) => allUniqueKeys.add(key));
-
-  return allUniqueKeys;
-};
-
 const isKeyRemoved = (key, obj1, obj2) => _.has(obj1, key) && !_.has(obj2, key);
 const isKeyAdded = (key, obj1, obj2) => !_.has(obj1, key) && _.has(obj2, key);
 const isValueEqual = (key, obj1, obj2) => obj1[key] === obj2[key];
@@ -32,31 +21,44 @@ const getKeyStatus = (key, obj1, obj2) => {
 };
 
 const map = (obj1, obj2, key) => {
-  switch (getKeyStatus(key, obj1, obj2)) {
+  const keyStatus = getKeyStatus(key, obj1, obj2);
+
+  switch (keyStatus) {
     case '-':
       return {
-        status: '-',
+        status: keyStatus,
         value1: obj1[key],
       };
     case '+':
       return {
-        status: '+',
+        status: keyStatus,
         value2: obj2[key],
       };
     case '=':
       return {
-        status: '=',
+        status: keyStatus,
         value1: obj1[key],
       };
     case '≠':
       return {
-        status: '≠',
+        status: keyStatus,
         value1: obj1[key],
         value2: obj2[key],
       };
     default:
       throw new Error('error while mapping');
   }
+};
+
+const createSetOfAllKeys = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  const allUniqueKeys = new Set();
+  keys1.forEach((key) => allUniqueKeys.add(key));
+  keys2.forEach((key) => allUniqueKeys.add(key));
+
+  return allUniqueKeys;
 };
 
 const createDiffTree = (obj1, obj2) => {
