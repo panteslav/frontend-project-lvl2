58edca3ed57c5d9e2cf5316f4f29c6e517272a15
panteslav <panteslav@users.noreply.github.com>
2020-09-09 22:08:46 +0300

    function name fixed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import createDiffTree from './differenceGenerator';
 import getFormatter from './formatters/index.js';
 
 const gendiff = (path1, path2, format = 'stylish') => {
-  const printFormatted = getFormatter(format);
+  const formattedTree = getFormatter(format);
 
   const configuration1 = parse(path1);
   const configuration2 = parse(path2);
 
   const diffTree = createDiffTree(configuration1, configuration2);
 
-  return printFormatted(diffTree);
+  return formattedTree(diffTree);
 };
 
 export default gendiff;
