3d15b56a8d4ed33d86a576e0ec99addab54a0a56
panteslav <panteslav@users.noreply.github.com>
2020-10-13 03:00:53 +0300

    refactoring to remove unnecessary getStyledEntry function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -20,39 +20,34 @@ const getStyledValue = (value, indentSize) => {
   return getStyledResult(result, bracketIndent);
 };
 
-const getStyledEntry = (entryData, textIndent, indentSize) => {
-  const { key, status } = entryData;
-
-  const value1 = _.has(entryData, 'value1') ? getStyledValue(entryData.value1, indentSize) : null;
-  const value2 = _.has(entryData, 'value2') ? getStyledValue(entryData.value2, indentSize) : null;
-
-  switch (status) {
-    case 'removed':
-      return `${textIndent}- ${key}: ${value1}`;
-    case 'added':
-      return `${textIndent}+ ${key}: ${value2}`;
-    case 'equal':
-      return `${textIndent}  ${key}: ${value1}`;
-    case 'modified':
-      return `${textIndent}- ${key}: ${value1}\n${textIndent}+ ${key}: ${value2}`;
-    default:
-      throw new Error(`unknown value status: ${status}`);
-  }
-};
-
 const getStyledTree = (tree, indentSize = BASE_INDENT_SIZE) => {
   const bracketIndent = ' '.repeat(indentSize - BASE_INDENT_SIZE);
   const textIndent = ' '.repeat(indentSize - SYMBOL_COMPENSATION_INDENT_SIZE);
 
   const result = tree.map((entryData) => {
     const currentIndentSize = indentSize + BASE_INDENT_SIZE;
-    if (entryData.status === 'complex') {
-      return `${textIndent}  ${entryData.key}: ${getStyledTree(
-        entryData.children,
-        currentIndentSize,
-      )}`;
+    const { key, status, children } = entryData;
+
+    const value1 = _.has(entryData, 'value1') ? getStyledValue(entryData.value1, currentIndentSize) : null;
+    const value2 = _.has(entryData, 'value2') ? getStyledValue(entryData.value2, currentIndentSize) : null;
+
+    switch (status) {
+      case 'complex':
+        return `${textIndent}  ${key}: ${getStyledTree(
+          children,
+          currentIndentSize,
+        )}`;
+      case 'removed':
+        return `${textIndent}- ${key}: ${value1}`;
+      case 'added':
+        return `${textIndent}+ ${key}: ${value2}`;
+      case 'equal':
+        return `${textIndent}  ${key}: ${value1}`;
+      case 'modified':
+        return `${textIndent}- ${key}: ${value1}\n${textIndent}+ ${key}: ${value2}`;
+      default:
+        throw new Error(`unknown value status: ${status}`);
     }
-    return getStyledEntry(entryData, textIndent, currentIndentSize);
   });
 
   return getStyledResult(result, bracketIndent);
